7705a674de4e436c187ebccf1f393e1c08011b3a
Gilbert Malova <malovagilbert@gmail.com>
2022-12-06 11:37:02 +0300

    Assigment 1 solution 1

---
+++ Assignment 1/iteration.js
@@ -0,0 +1,16 @@
+function fibs(n) {
+  let arr = [0,1];
+  if (n === 1) {
+    return arr[0]
+  } else if (n === 2) {
+    return arr;
+  }
+  else if (n === 0) {
+    return 'Error'
+  }
+  for (let i = 2; i < n; i++) {
+    arr.push( arr[arr.length - 1] + arr[arr.length-2])
+  }
+  return arr;
+};
+console.log(fibs(5))
